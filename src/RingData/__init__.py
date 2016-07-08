bf6ba6648a52dfcd707709240e031d6198eee8fc
dermen <dermen@stanford.edu>
2016-07-08 08:45:08 -0700

    moved PolarTree (K-D tree) class to RingData.RingFetch

--- src/RingData/__init__.py
+++ src/RingData/__init__.py
@@ -2,6 +2,7 @@
 
 from .RingFetch import RingFetch
 from .RingFetch import InterpSimple
+from .RingFetch import PolarTree
 from .RadialProfile import RadialProfile
 from .DiffCorr import DiffCorr
 from .RingFit import RingFit
