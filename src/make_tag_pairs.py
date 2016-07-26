202a3e16ced73725cea97993f2d50586d8a7db9a
dermen <dermen@stanford.edu>
2016-07-25 19:22:20 -0700

    new make tag pairs routines

--- src/make_tag_pairs.py
+++ src/make_tag_pairs.py
@@ -1,61 +1,63 @@
-import sys
-import collections
 
-import json
+import h5py
 import pandas
 import numpy as np
+from scipy.spatial import distance
 
 from loki.utils import postproc_helper as helper
+#from loki.utils import stable
+import stable
 
 class MakeTagPairs:
-    def __init__(self, db_pickle, fixed_qr=False, qrmin=None, 
-                    qrmax=None, dqr=1, poly_stride=10, rm_mean_outliers=True,
-                    rm_mean_thresh=3, min_grp_size=20, signal=None, 
-                    min_signal = None, max_signal = None):
+
+    def __init__(self, db_pickle, fixed_qr=False, qrmin=None,
+                 qrmax=None, dqr=1, poly_stride=10, rm_mean_outliers=True,
+                 rm_mean_thresh=3, min_grp_size=20, signal=None,
+                 min_signal=None, max_signal=None):
         """
         'db_pickle' - pandas pickle file from make_db.MakeDatabase
         'fixed_qr' - whether the radial position of each q changing from
-                     shot to shot (e.g. if used pk_detect=True 
+                     shot to shot (e.g. if used pk_detect=True
                      in MakeDatabase
-        
+
         If the qr is fluctating, this will group shots according to their
             pk_pos (q  of max intensity along radial profile), and then
             pairings will only be considered for exposures within the same
             group
-        
+
         'qrmin'     - min q to form into groups
         'qrmax'     - max q to form into groups
         'dqr'           - how wide the qr bins are , default is 1
-        'min_grp_size' - if grouping according to qr, how big should a 
-                        group be in order to be analyzed 
+        'min_grp_size' - if grouping according to qr, how big should a
+                        group be in order to be analyzed
 
-        If a shot has pk_pos < qrmin or pk_pos > qrmax, then the shot will 
+        If a shot has pk_pos < qrmin or pk_pos > qrmax, then the shot will
         not be included in the analysis
-        
+
         'rm_mean_outliers' - whether to remove shot mean outliers
         'rm_mean_thresh'   - threshhold parameter for removing shots
                             make this lower to remove more shots
 
         'poly_stride' - resolution parameter for computing polynomials along
-                     0-2PI 
+                     0-2PI
                     (used for poly comparison of shots)
                 default is 10, so the res will be   10 * 2pi/nphi
-                make this number higher to speed up comparison computation, 
+                make this number higher to speed up comparison computation,
                 but dont make it too high (has'nt been tested)
         'signal'       - tells what kind of signal should be thresholded
                         can be ['mean', 'stdev', 'snr' ]
-        'min_signal'   - minimum signal (as determined by the 'signal' 
-                        parameter) 
+        'min_signal'   - minimum signal (as determined by the 'signal'
+                        parameter)
         'max_signal'   - maximum signal (as determined by the 'signal'
-                         parameter) 
+                         parameter)
                         for a shot to be considered for analysis
         """
         self.db_pickle = db_pickle
-        print ("\nLoading pandas pickle: %s"%self.db_pickle)
-        self.df = pandas.read_pickle( self.db_pickle)
+        print ("\nLoading pandas pickle: %s" % self.db_pickle)
+        self.df = pandas.read_pickle(self.db_pickle)
         self.fixed_qr = fixed_qr
         self.nphi = self.df.num_phi[0]
-        
+
         if self.fixed_qr:
             print ("Will not form radial groups before pairings...")
         else:
@@ -67,107 +69,105 @@ class MakeTagPairs:
         self.qrmin = qrmin
         self.qrmax = qrmax
         self.dqr = dqr
-        self.poly_stride = poly_stride
         self.min_grp_size = min_grp_size
         self._make_signal_column(signal)
         if signal is not None:
-            self._filter_from_signal( min_signal)
+            self._filter_from_signal(min_signal)
 
 #       exclude exposures with a high/low mean intensity
         if rm_mean_outliers:
             print ("Removing shot mean outliers...")
             self._drop_shotmean_outliers(thresh=rm_mean_thresh)
-        
+
+        self.df.reset_index(inplace=True)
         self.groups = None
         self.u_gruops = None
-        if not fixed_qr: 
+        if not fixed_qr:
             self._group_shots_by_peakpos()
 
     def _make_signal_column(self, signal):
-        assert( signal in [None, 'mean', 'stdev', 'snr'] )
+        assert(signal in [None, 'mean', 'stdev', 'snr'])
 #           whether to apply a secondary signal threshhold
-        if signal=='mean':
+        if signal == 'mean':
             print("Will use the shot mean filter...")
-            self.df['sig'] = self.df.shot_mean 
+            self.df['sig'] = self.df.shot_mean
         elif signal == 'stdev':
             print("Will use the shot standard deviation filter...")
             self.df['sig'] = self.df.shot_stdev
-        elif signal== 'snr':
-            print ( "Will use signal to noise filter, be sure you used the",\
-                    "pk_detect option when making the database or this",\
-                    "will break.")
-            assert( self.df.pk_amp[0] is not None and \
-                        self.df.background_offset is not None )
+        elif signal == 'snr':
+            print ("Will use signal to noise filter, be sure you used the",
+                   "pk_detect option when making the database or this",
+                   "will break.")
+            assert(self.df.pk_amp[0] is not None and
+                   self.df.background_offset is not None)
             self.df['sig'] = self.df.pk_amp / self.df.background_offset
 
-    
     def _filter_from_signal(self, min_signal):
         n_total = len(self.df)
         if min_signal is not None:
-            self.df = self.df.query('sig >= %f'%min_signal )
+            self.df = self.df.query('sig >= %f' % min_signal)
         if max_signal is not None:
-            self.df = self.df.query('sig <= %f'%max_signal )
+            self.df = self.df.query('sig <= %f' % max_signal)
         n_removed = n_total - len(self.df)
-        print ("Removed %d/%d exposures from analysis"%(n_removed,
-                                                        n_total))
-    
+        print ("Removed %d/%d exposures from analysis" % (n_removed,
+                                                          n_total))
+
     def _drop_shotmean_outliers(self, thresh):
-        outliers = helper.is_outlier( self.df.shot_mean.values, thresh )
+        outliers = helper.is_outlier(self.df.shot_mean.values, thresh)
         where_outliers = np.where(outliers)[0]
         if where_outliers.size:
             n_total = len(self.df)
-            self.df.drop( self.df.index[where_outliers], inplace=True)
-            n_removed = n_total-len(self.df)
-            print ("Removed %d/%d exposures from analysis"%(n_removed,
-                                                            n_total))
+            self.df.drop(self.df.index[where_outliers], inplace=True)
+            n_removed = n_total - len(self.df)
+            print ("Removed %d/%d exposures from analysis" % (n_removed,
+                                                              n_total))
 
     def _group_shots_by_peakpos(self):
         """group according to pk_pos parameter, e.g. fitted q111"""
         print ("grouping shots according to radial intensity peak position.")
-        qbins = np.arange( self.qrmin, self.qrmax, self.dqr)
-        self.groups = np.digitize( self.df.pk_pos, bins=qbins )
-        self.u_groups = np.unique(self.groups) 
+        qbins = np.arange(self.qrmin, self.qrmax, self.dqr)
+        self.groups = np.digitize(self.df.pk_pos, bins=qbins)
+        self.u_groups = np.unique(self.groups)
 
-    def Make(self, outfile, poly_thresh=3.5):
+    def Make(self, outfilename, poly_thresh=3.5):
         """
         make the tag pairings
 
-        outfile - name f the json file which will store the pairings
+        outfilename - name f the hdf5 file which will store the pairings
         poly_thresh - threshhold for detecting outlier chi-square deltas
-                        between exposures. If 
+                        between exposures. If
                                 min_s,s'( chi-square)
-                        is still too large, then the pair s,s' wont be 
-                        considered. 
+                        is still too large, then the pair s,s' wont be
+                        considered.
         """
-        self.outfile = outfile
-        self.poly_thresh = poly_thresh
+        self.outfile = h5py.File( outfilename, 'w')
 
+        self.poly_thresh = poly_thresh
         self.tag_pairs = []
-        self.eps_min = []
-        if not self.fixed_qr:        
+        if not self.fixed_qr:
             print ("Iterating over groups and pairing")
-            self._iterate_groups() 
+            self._iterate_groups()
 
         else:
             print ("Pairing all remaining shots in database")
             self.df_g = self.df
+            self.groupID = 0
             self._pair_group()
-            self._find_min_pairs()
-        
-        self._save_tag_pairs()
+
+        self.outfile.close()
+
 
     def _iterate_groups(self):
         """
         Iterate over groups, determined from _group_shots_by_peakpos,
         pairing each group
         """
-
-        for g in self.u_groups:
-            self.df_g = self.df.loc[ self.groups==g ]
+        for self.groupID, g in enumerate(self.u_groups):
+            self.df_g = self.df.loc[self.groups == g]
             if len(self.df_g) < self.min_grp_size:
-                continue 
-            self._pair_group()    
-            self._find_min_pairs()
+                continue
+            self.df_g.reset_index(inplace=True)
+            self._pair_group()
 
     def _pair_group(self):
         """
@@ -175,72 +175,73 @@ class MakeTagPairs:
         similarity of their respective polynomials
         """
         ngroup = len(self.df_g)
-       
-        # polynomial at lower resolution than the ring (it is smooth)
-        Px = np.arange(self.nphi)[::self.poly_stride]
-        Py = np.vstack( [np.polynomial.chebyshev.chebval(Px,c) 
-                            for c in self.df_g.cheby_fit_pkremove])
-                            #for c in self.df_g.cheby_fit_compare])
+        Px = np.arange(self.nphi)
+        Py = np.vstack([np.polynomial.chebyshev.chebval(Px, c)
+                        for c in self.df_g.cheby_fit_pkremove])
+
+#       polynomial distance measure between shot i and shot j
+        print("  Calculating the distance matrix...")
+        eps = distance.cdist(Py, Py, metric='euclidean')
         
-        # polynomial distance measure between shot i and shot j
-        eps = np.array( [ [sum((Py[i]-p)**2)
-                             for p in Py ] 
-                          for i in xrange(ngroup)]  )
-       
-        # Add the max to the diagonal, otherwise the diagonal will 
-        # register as the minimum 
-        epsI = eps.max(1)*np.identity(ngroup)
+        print("  Calculating the preference matrix...")
+        # Add the max to the diagonal, otherwise the diagonal will
+        # register as the minimum (each shot is closest to itself)
+        epsI = 1.1*eps.max(1) * np.identity(ngroup)
         eps += epsI
+
+#       column 0 is the shot, columns 1->N are the "closest"
+#       shots, 1 being the closest and N being the furthest
+        self._shot_preference = np.roll(eps.argsort(1), 1, axis=1)
+
+        print("    storing in hash table...")
+        pref_dict = {str(E[0]): list(E[1:]) 
+            for E in self._shot_preference.astype(str) }
+
+#       use the stable roommate (Irving's) algorithm to pair the shots
+        print("  Forming the pairings using Irving's algorthm...")
+        pairs_dict = stable.stableroomate(prefs=pref_dict)
         
-        self.eps_order = np.argsort( eps,1)
+        pairs = self._remove_duplicate_pairs( pairs_dict)
+
+        tag_pairs = [ (str(self.df_g.tag[i]), str(self.df_g.tag[j]) ) 
+            for i,j in pairs]
+
+        self.tag_pairs.extend(  tag_pairs )
+
+        pdists = [ eps[i][j] for i,j in pairs ]
+        score = np.mean(pdists)/np.mean(eps.ravel())
+
+        print("  Pairing score for group %d: %.3f"%(self.groupID, score))
+
+        self.outfile.create_dataset('group%d/pair_distances'%self.groupID, data=pdists)
+        self.outfile.create_dataset('group%d/tag_pairs'%self.groupID, data=tag_pairs)
+        self.outfile.create_dataset('group%d/pairs'%self.groupID, data=pairs)
+        self.outfile.create_dataset('group%d/score'%self.groupID, data=score)
+        self.outfile.create_dataset('group%d/distance_matrix'%self.groupID, data=eps)
+
+    @staticmethod
+    def _remove_duplicate_pairs(pairs_dict):
+        pairs = []
+        for k,v in pairs_dict.iteritems():
+            set_p = set((int(k), int(v)))
+            if set_p not in pairs:
+                pairs.append( set_p )
+        return [ [i,j] for i,j in pairs ]
 
 
-    def _find_min_pairs(self):
-        pair_inds = []
-        for i in xrange(len(self.eps_order) ):
-            min_row = list(self.eps_order[i])
-            min_pair = [i, min_row.pop(0) ]
-            while np.any([ind in pair_inds 
-                            for ind in min_pair]):
-                try:
-                    min_pair = [i, min_row.pop(0)]
-                except IndexError:
-                    min_pair = None
-                    break
-            if min_pair is not None and len(set(min_pair)) > 1:
-                pair_inds.extend(min_pair)
-#               min_pair is indexed from 0 to num of shots in group-outlier. 
-#               df_g.tag.keys() are still indexed from 0 to the original number of shots
-                min_pair_key=[ self.df_g.tag.keys()[this_pair] for this_pair in min_pair]
-                tag_pair = map(str, self.df_g.tag[min_pair_key])
-                self.tag_pairs.extend( tag_pair)
+def pair_shots(  eps ):
+    #epsI = 1.1*eps.max(1) * np.identity(eps.shape[0])
+    #eps += epsI
+    shot_preference = np.roll(eps.argsort(1), 1, axis=1)
+    pref_dict = {str(E[0]): list(E[1:]) 
+        for E in shot_preference.astype(str) }
 
-    
-    def _save_tag_pairs(self):
-        """
-        saves the tag pairings list in a json file
-        """
-        self.tag_pairs = zip( self.tag_pairs[::2], 
-                                    self.tag_pairs[1::2] )
-        print ("Made %d tag pairings..."%len(self.tag_pairs))
-        outfile_ = open( self.outfile, 'w')
-        json.dump( self.tag_pairs, outfile_)
-        outfile_.close()
-        print ("Saved tag pairings in json file: %s"%self.outfile)
+    pairs_dict = stable.stableroomate(prefs=pref_dict)
+    return pairs_dict
+
 
 if __name__ == '__main__':
     db_pickle = 'test.pkl'
-    outf = 'test_pkremove.json'    
-#   qrmin = 20 # min q radius relative to qmin (pixel units)
-#   qrmax = 60 # max q ..                  
-#   makeTagPairs = MakeTagPairs(db_pickle, qrmin=qrmin, dqr=3, 
-#                        qrmax=qrmax, signal='mean', min_signal=325,
-#                        rm_mean_thresh=5, max_signal=3300)
-    
-#    makeTagPairs.Make(outf)
-        
-    makeTagPairs = MakeTagPairs(db_pickle, fixed_qr=True,poly_stride=2) 
-                                #rm_mean_outliers=True,
-                                #rm_mean_thresh=5, poly_stride=2)
+    outf = 'test_pairs.hdf5'
+    makeTagPairs = MakeTagPairs(db_pickle, fixed_qr=True)
     makeTagPairs.Make(outf)
-
---
+++ src/utils/stable.py
@@ -0,0 +1,414 @@
+###############################################################
+# THIS CODE BORROWED FROM:
+# https://github.com/CoeCS/tacklebox/tree/master/stableroomate
+###############################################################
+
+import csv
+import random
+import sys
+import optparse
+import logging
+
+
+log = logging.getLogger("stableroommate")
+
+def readprefs(prefsfn):
+    """
+    read the preferences from "prefs.csv"
+    """
+    inner = csv.reader(open(prefsfn))
+
+    prefs = {}
+
+
+    for line in inner:
+        if len(line) == 0: continue
+        line = [s.strip() for s in line]
+        prefs[line[0]] = line[1:]
+
+    return prefs
+
+def fillin(prefs):
+    """
+    some choices may not include everyone.
+
+    fill in the rest by rearranging the others available at random.
+    """
+
+    names = set(prefs.keys())
+
+    for name, choices in prefs.iteritems():
+
+        left = set(names).difference([name]).difference(choices)
+
+
+        if len(left) > 0:
+            left = list(left)
+            random.shuffle(left)
+            choices.extend(left)
+
+def checkprefs(prefs):
+    """
+    - `prefs`: preferences dict, name key, list of names as choices in order
+    """
+
+    names = set(prefs.keys())
+
+    for name, choices in prefs.iteritems():
+        try:
+            assert len(names.difference(choices)) == 1
+        except AssertionError, e:
+            log.alert("len(names.difference(choices)) = {0} != 0".format(
+                len(names.difference(choices))))
+            log.alert(name, choices)
+            log.alert(names.difference(choices))
+
+            raise AssertionError(e)
+
+
+        try:
+            assert len(choices) == len(names) - 1
+        except AssertionError, e:
+            log.critical("len(choices) != len(names) - 1")
+            log.critical("{0} != {1}".format(len(choices), len(names)-1))
+            log.critical("{0} {1}".format(name, choices))
+            raise AssertionError(e)
+
+
+def verify_ranks(ranks, prefs):
+    """
+    check that ranks and prefs correspond
+
+    Arguments:
+    - `ranks`: dict mapping name to rank index
+    - `prefs`: preferences dict, name key, list of names as choices in order
+    """
+
+    for n in ranks:
+        for m in ranks[n]:
+            idx = ranks[n][m]
+            try:
+                assert m == prefs[n][idx]
+            except AssertionError(e):
+                log.critical("m != prefs[n][idx]")
+                log.critical("{0} != {1}".format(m, prefx[n][idx]))
+                raise AssertionError(e)
+
+
+def reject(prefs, ranks, holds):
+    """
+    This does a reduction of the ranks if either of the following conditions
+    holds.
+
+    (i)
+
+    (ii)
+    """
+
+    for y in holds:
+
+        # n holds holds[n]
+        i = 0
+        x = holds[y]
+        while i < len(prefs[y]):
+            yi = prefs[y][i]
+
+            if yi == x:
+                prefs[y] = prefs[y][:i+1]
+
+            # lower rank is better
+            elif ranks[yi][holds[yi]] < ranks[yi][y]:
+                prefs[y].pop(i)
+                continue
+            i += 1
+
+
+
+def find_all_or_nothing(prefs, ranks, holds):
+    """
+    Find an all or nothing cycle.
+
+    Arguments:
+    - `prefs`:
+    - `ranks`:
+    - `holds`:
+    """
+    p = []
+    q = []
+
+    # first find a key that has more than one pref left
+    for x in sorted(prefs):
+        if len(prefs[x]) > 1:
+            cur = x
+            break
+    else:
+        return None
+
+
+    # trace through
+    while cur not in p:
+        # q_i = second person in p_i's list
+        q.append( prefs[cur][1] )
+
+        # p_{i+1} = q_i' last person
+        p.append(cur)
+        cur = prefs[q[-1]][-1]
+
+    a = p[p.index(cur):]
+    b = [prefs[n][0] for n in a]
+
+    return a
+
+
+
+def phase1(prefs, ranks, curpref=None, debug=False):
+    """
+    perform phase 1 of the stable roomates problem.
+
+    Arguments:
+    - `prefs`: preferences dict, name key, list of names as choices in order
+    - `ranks`: dict mapping name to rank index
+    """
+
+    # holds
+    holds = dict( (name,None) for name in prefs.keys() )
+
+    if curpref is None:
+        curpref = dict( (name, 0) for name in prefs.keys() )
+
+    people = prefs.keys()
+    random.shuffle(people)
+
+    proposed_to = set()
+
+    log.info("-- phase 1 -----------")
+    log.debug("{0}".format(people))
+
+
+    for person in people:
+        poser = person
+
+        while (1):
+            # find poser someone
+            while curpref[poser] < len(prefs[poser]):
+                # person poser is proposing to
+                nchoice = prefs[poser][curpref[poser]]
+                curpref[poser] += 1
+
+                # person poser is holding
+                cchoice = holds[nchoice]
+                log.info("{0} proposes to {1};".format(poser,nchoice))
+
+
+                # lower ranking is better
+                if cchoice is None or \
+                        ranks[nchoice][poser] < ranks[nchoice][cchoice]:
+                    break
+                log.info("{0} rejects {1};".format(nchoice, poser))
+
+
+            log.info("{0} holds {1}".format(nchoice, poser))
+            holds[nchoice] = poser
+
+            if nchoice not in proposed_to:
+                log.info("done")
+                assert cchoice is None
+                break
+
+            log.info("and rejects {0}".format(cchoice))
+            poser = cchoice
+
+        proposed_to.add(nchoice)
+
+    return holds
+
+
+
+def stableroomate(prefs=None, prefsfn=None, debug=False):
+    """
+    find a stable roomate matching
+    """
+
+    # read prefs from file
+    if prefsfn is not None:
+        prefs = readprefs(prefsfn)
+    else:
+        assert( prefs is not None)
+
+    # make sure everyone has the same number of choices
+    fillin(prefs)
+
+
+    # validate that names are correct
+    checkprefs(prefs)
+
+    # generate a dictionary of rank values for each name
+    ranks = dict( (idx, dict(zip(val,range(len(val)) )))
+                 for idx,val in prefs.iteritems() )
+
+    # validate the ranks correspond to the proper indices
+    verify_ranks(ranks, prefs)
+
+    # phase1
+    holds = phase1(prefs, ranks)
+
+    log_holds(holds)
+
+    reject(prefs, ranks, holds)
+
+    cycle = find_all_or_nothing(prefs, ranks, holds)
+
+    if cycle is not None and  len(cycle) == 3:
+        print "no solution exists"
+        return
+
+    ## phase 2
+    while cycle is not None:
+        log.debug("-- cycle detected -----------")
+        log.debug("{0}".format(cycle))
+
+        curpref = {}
+        for x in prefs:
+            if x in cycle:
+                curpref[x] = 1
+            else:
+                curpref[x] = 0
+
+        holds = phase1(prefs, ranks, curpref)
+
+        log_holds(holds)
+
+        reject(prefs, ranks, holds)
+
+        cycle = find_all_or_nothing(prefs, ranks, holds)
+
+
+
+    # print prefs
+    # print ranks
+    return holds
+
+def log_holds(holds):
+    """
+
+    Arguments:
+    - `holds`:
+    """
+
+    log.info("-- holds -----------")
+    for h in holds:
+        log.info("{0} {1}".format(h, holds[h]))
+
+
+
+def log_prefs(prefs):
+    """
+
+    Arguments:
+    - `prefs`:
+    """
+
+    log.info("-- prefs -----------")
+    for x in sorted(prefs):
+        log.info("{0}\t{1}".format(x, " ".join(prefs[x])))
+
+
+def swap_better(set1, set2, ranks):
+    """
+    """
+
+    x1, y1 = set1
+
+    x2, y2 = set2
+
+    x1y1 = ranks[x1][y1]
+    y1x1 = ranks[y1][x1]
+    x2y2 = ranks[x2][y2]
+    y2x2 = ranks[y2][x2]
+
+    x1x2 = ranks[x1][x2]
+    x2x1 = ranks[x2][x1]
+    y1y2 = ranks[y1][y2]
+    y2y1 = ranks[y2][y1]
+
+    x2y1 = ranks[x2][y1]
+    y1x2 = ranks[y1][x2]
+    x1y2 = ranks[x1][y2]
+    y2x1 = ranks[y2][x1]
+
+    if x1x2 < x1y1 and x2x1 < x2y2 and y1y2 < y1x1 and y2y1 < y2x2:
+        log.error("({0},{1}) ({2},{3}) -> ({4},{5}) ({6},{7})".format(
+            x1, y1, x2, y2, x1, x2, y1, y2))
+        log.error("({0},{1}) ({2},{3}) -> ({4},{5}) ({6},{7})".format(
+            x1y1, y1x1, x2y2, y2x2, x1x2, x2x1, y1y2, y2y1))
+
+    if x2y1 < x2y2 and y1x2 < y1x1 and x1y2 < x1y1 and y2x1 < y2x2:
+        log.error("({0},{1}) ({2},{3}) -> ({4},{5}) ({6},{7})".format(
+            x1, y1, x2, y2, x1, y2, x2, y1))
+        log.error("({0},{1}) ({2},{3}) -> ({4},{5}) ({6},{7})".format(
+            x1y1, y1x1, x2y2, y2x2, x1y2, y2x1, x2y1, y1x2))
+
+
+
+def verify_match(matches):
+    """
+    """
+
+    #prefsfn = sys.argv[1]
+
+    # read prefs from file
+    prefs = readprefs(prefsfn)
+    fillin(prefs)
+
+    # generate a dictionary of rank values for each name
+    ranks = dict( (idx, dict(zip(val,range(len(val)) )))
+                 for idx,val in prefs.iteritems() )
+
+    for x in matches:
+        for y in matches:
+            if y == x or y == matches[x]:
+                continue
+
+            set1 = (x, matches[x])
+            set2 = (y, matches[y])
+
+            swap_better(set1, set2, ranks)
+
+def main():
+    """
+    main function
+    """
+
+    # random.seed(1000)
+
+    parser = optparse.OptionParser(usage="usage: %prog [options] prefsfn")
+    parser.add_option("-v", dest="validate", action="store_true",
+                      default=False, help="Validate the Algorithm")
+    parser.add_option("-d", dest="debug", action="store_true",
+                      default=False, help="Print Debuggin Code")
+    (options, args) = parser.parse_args()
+
+
+    if options.debug:
+        logging.basicConfig(level=logging.DEBUG)
+
+    if len(args) < 1:
+        parser.print_help()
+        return
+
+    matches = stableroomate(args[0], options.debug)
+
+    if matches is not None:
+        print("-- matches -----------")
+        for m in matches:
+            print "{0} {1}".format(m, matches[m])
+
+        if options.validate:
+            log.info("verifying matches...")
+            verify_match(matches)
+
+
+
+if __name__ == '__main__':
+    main()
+
