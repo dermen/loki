41d7f0c7506e336e6aa26123c98b1693ec7eabae
Shenglan Qiao <qiaoshen@pslogin7d.slac.stanford.edu>
2017-10-24 17:04:37 -0700

    script used to experiment radial profile clustering on water

--- lcls_scripts/water_cluster_dif_cor.py
+++ lcls_scripts/water_cluster_dif_cor.py
@@ -12,10 +12,13 @@ import matplotlib.pyplot as plt
 
 # load the water run
 f = h5py.File('/reg/d/psdm/cxi/cxilp6715/scratch/combined_tables/finer_q/run94.tbl','r')
-f_out = h5py.File('/reg/d/psdm/cxi/cxilp6715/scratch/water_data/run94_dif_int_basic_pmask_fd.h5','w')
+f_out = h5py.File('/reg/d/psdm/cxi/cxilp6715/scratch/water_data/run94_dif_int_basic_pmask_removePeak.h5','w')
 #######################
 use_basic_mask= True
-pmask_basic = np.load('/reg/d/psdm/cxi/cxilp6715/scratch/water_data/binned_pmask_basic.npy')
+if 'polar_mask_binned' in f.keys():
+    pmask_basic = np.array(f['polar_mask_binned'].value==f['polar_mask_binned'].value.max(), dtype = int)
+else:
+    pmask_basic = np.load('/reg/d/psdm/cxi/cxilp6715/scratch/water_data/binned_pmask_remove_peak.npy')
 #######################
 PI = f['polar_imgs']
 
@@ -53,8 +56,9 @@ for cc in unique_clusters:
         break
 
 # sub cluster by max height
-shot_set_num = 0
-
+# shot_set_num = 0
+shot_nums_per_set = []
+norm_corrs = []
 print("sorting shots into clusters...")
 for cluster_num in cluster_to_use:
     shot_tags = np.where(pulse_energy_clusters==cluster_num)[0]
@@ -89,14 +93,13 @@ for cluster_num in cluster_to_use:
             shots = shots.astype(np.float64)
         
         norm_shots = np.zeros_like(shots)
-        masked_shots = np.zeros_like(shots)
+
         for idx, ss in enumerate(shots):
             if use_basic_mask:
                 mask = pmask_basic
             else:
                 mask = make_mask(ss,zero_sigma=0.0)
             ss *=mask
-            masked_shots[idx] = ss
 
             mean_ss = ss.sum(-1)/mask.sum(-1) 
 
@@ -113,15 +116,31 @@ for cluster_num in cluster_to_use:
             sorted_shots = sorted_shots[:-1]
         diff_norm = sorted_shots[1::2]-sorted_shots[::2]
 
-        sorted_shots = masked_shots[order]
-        if sorted_shots.shape[0]%2>0:
-            sorted_shots = sorted_shots[:-1]
-        diff_masked = sorted_shots[1::2]-sorted_shots[::2]
 
+        # dummy qvalues
+        qs = np.linspace(0.1,1.0, diff_norm.shape[1])
+        dc = DiffCorr(diff_norm, qs, 0,pre_dif=True)
+        ac = dc.autocorr().mean(0)
+        norm_corrs.append(ac)
+        shot_nums_per_set.append(diff_norm.shape[0])
+    
         # save difference int
-        f_out.create_dataset('norm_diff_%d'%shot_set_num, data = diff_norm)
-        f_out.create_dataset('masked_diff_%d'%shot_set_num, data = diff_masked)
-        shot_set_num+=1
+        # f_out.create_dataset('norm_diff_%d'%shot_set_num, data = diff_norm)
+
+        # shot_set_num+=1
+
+
+ave_norm_corr = (norm_corrs * \
+    (np.array(shot_nums_per_set)/float(np.sum(shot_nums_per_set)))[:,None,None]).sum(0)
+if use_basic_mask:
+    qs = np.linspace(0.1,1.0, diff_norm.shape[1])
+    mask_dc = DiffCorr(mask[None,:], qs, 0, pre_dif=True)
+    mask_corr = mask_dc.autocorr().mean(0)
+    ave_norm_corr /= mask_corr
+
+f_out.create_dataset('ave_norm_corr',data=ave_norm_corr)
+f_out.create_dataset('num_shots',data=np.sum(shot_nums_per_set))
+
 
 f_out.close()
 
