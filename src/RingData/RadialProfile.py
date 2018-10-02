686707716107f08b653dd65781cd12299d8e749f
dermen <dermen@stanford.edu>
2018-10-01 20:50:09 -0500

    added maximum histogram bin functionalty

--- src/RingData/RadialProfile.py
+++ src/RingData/RadialProfile.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import binned_statistic
 
 from loki.RingData import RingFetch
 
@@ -19,7 +20,6 @@ class RadialProfile:
         minlength:  The minimum length of the radial profile. Force this to be
                     high so that multiple radial profiles will have
                     the same length
-
         """
         self.x_center = center[0]
         self.y_center = center[1]
@@ -30,17 +30,27 @@ class RadialProfile:
         self.Y, self.X = np.indices( img_shape )
         self._set_R()
         self._set_normalization()
-        
+        self._set_R_sorted_idx()
+
         self.wavelen = None
         self.pixsize = None
         self.detdist = None
         self.factor = None
 
+    def _set_R_sorted_idx(self):
+        self.R1 = self.R.ravel()
+        self.sort_idx = np.argsort( self.R1 )
+        Rsort = self.R1[self.sort_idx]
+        self.Rsort_bin_edge = np.where( Rsort[1:] - Rsort[:-1] == 1)[0] + 1
+        self.Rsort_bin_edge =  np.array( \
+            [0] + list( self.Rsort_bin_edge) + [ len( self.R1) ] )
+    
     def _set_R(self):
         self.R = np.sqrt((self.Y-self.y_center)**2 + \
                         (self.X-self.x_center)**2)
         self.R = self.R.astype(int)
-        
+        self.Rbins = np.arange( self.R.max()+1) 
+    
     def _set_normalization(self):
         if self.mask is None:
             self.num_pixels_per_radial_bin = np.bincount( self.R.ravel(), 
@@ -69,7 +79,48 @@ class RadialProfile:
                             self.num_pixels_per_radial_bin
 
         return np.nan_to_num(radial_profile)
+    
+    def calculate2(self, img, how="mean", bins=None):
+        """
+        img:   2-dimensional image to be radially binned
+        how:   what metric to use
+        bins:  which bins to use, only applies to how=='max' case
+        returns the 1-dimensional radial profile of img
+        """
+        if how=="mean":
+            if self.mask is None:
+                summed_intensity_per_radial_bin = np.bincount( self.R.ravel(), 
+                                                    weights=img.ravel(), 
+                                                    minlength=self.minlength)
+            else:
+                summed_intensity_per_radial_bin = np.bincount( self.R.ravel(), 
+                                                    weights=(self.mask*img).ravel(), 
+                                                    minlength=self.minlength)
 
+            radial_profile = summed_intensity_per_radial_bin / \
+                                self.num_pixels_per_radial_bin
+        elif how=="max":
+            if bins is not None:
+                result =  binned_statistic( self.R.ravel(), values=img.ravel(), 
+                                                bins=bins, statistic='max' )
+            else:
+                result =  binned_statistic( self.R.ravel(), values=img.ravel(), 
+                                                bins=self.Rbins, statistic='max' )
+            radial_profile = result.statistic
+        
+        elif how=='fast_max':
+            I1d = img.ravel()
+            #Nbin = len( self.sort_idx)
+            #radial_profile = np.zeros( Nbin)
+            radial_profile = [] 
+            for i_b,(b1,b2) in enumerate(zip( self.Rsort_bin_edge[:-1], self.Rsort_bin_edge[1:])):
+                radial_profile.append( max( I1d[ self.sort_idx[b1:b2] ] )) 
+
+        else:
+            print("how must be mean, max or fast_max")
+            return None
+
+        return np.nan_to_num(radial_profile)
 
     def calculate_using_fetch( self, img, radii, wavelen=None, 
             detdist=None, pixsize=None, factor=None, 
@@ -137,3 +188,4 @@ class RadialProfile:
         self.detdist = detdist
         self.pixsize = pixsize
         self.factor = factor
+
